421cc49169eed74de223871d3e294ff9fda12a47
Michael <michael.wiliams@colorado.edu>
2013-10-15 18:38:18 -0700

    Solution Attempt

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,6 +1,17 @@
 module MoviesHelper
   # Checks if a number is odd:
   def oddness(count)
-    count.odd? ?  "odd" :  "even"
+    count.odd? ? "odd" : "even"
+  end
+  # this is to just to get rid of repeating ourselves - checks for what box has been clicked on
+  def ratingsInclude(ratingName)
+    @checked_ratings.include?(ratingName) ? true : false
+  end
+  # creates our array so that we can sort in the haml and the movies_controlers
+  def createParamsArray(sortColumn)
+    @paramsArray = Hash.new
+    @paramsArray.merge!({:ratings => @checked_ratings_hash})
+    @paramsArray.merge!({:sort_by => sortColumn})
+    @paramsArray
   end
 end
